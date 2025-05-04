62dc539fc0ce4a80c0f6cac24321f2e4683c6053
ART-98 <artprogramming98@gmail.com>
2025-05-04 11:29:17 +0330

    mui remaining code removed

--- src/app/theme/theme.ts
+++
@@ -1,8 +0,0 @@
-import { createTheme } from "@mui/material/styles";
-
-const theme = createTheme({
-  direction: "rtl",
-  // Customize palette, typography, etc., here if needed
-});
-
-export default theme;
